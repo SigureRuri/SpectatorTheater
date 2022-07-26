57ae7af4a6fa9c01a92e07d91aea58199c4fa819
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-07-26 12:50:45 +0900

    fix: Bukkit.Worldではなくその名前で比較する

--- src/main/kotlin/com/github/sigureruri/spectatortheater/config/SpectatorTheaterConfig.kt
+++ src/main/kotlin/com/github/sigureruri/spectatortheater/config/SpectatorTheaterConfig.kt
@@ -1,11 +1,10 @@
 package com.github.sigureruri.spectatortheater.config
 
-import org.bukkit.Bukkit
 import org.bukkit.configuration.file.FileConfiguration
 
 class SpectatorTheaterConfig(config: FileConfiguration) {
 
-    val ENABLED_WORLDS = config.getStringList("enabled-worlds").mapNotNull { Bukkit.getWorld(it) }
+    val ENABLED_WORLDS = config.getStringList("enabled-worlds").mapNotNull { it.lowercase() }
 
     val COOL_TIME = config.getLong("cool-time")
 
